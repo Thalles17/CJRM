9f6605072ddfd1ae1b354ca3aa8f46c93c8d97f4
Thalles <thallesmanhaes17@gmail.com>
2023-06-04 20:24:07 -0300

    add os conceitos de dados boolean

--- aula-03/app.js
+++ aula-03/app.js
@@ -28,6 +28,52 @@ const arrayIcludes = names.includes('roger')//variavel mostra se tem o nome pass
 console.log(arrayIcludes)//true or false
 
 //Operadores de comparação
- 
+const age = 31
 
-//Operadores de comparação
+console.log(age == 31)//retorna true
+console.log(age == 35)//retorna false //igual a
+console.log(age != 35)//retorna true //não igual //diferente de
+console.log(age > 30)//retorna true //maior que
+console.log(age > 31)//retorna false //maior que
+console.log(age < 32)//retorna true //menor que 
+console.log(age <= 31)//retorna true //menor ou igual 
+console.log(age >= 31)//retorna true //maior ou igual
+
+//caracteres minusculos são maiores que caracteres maiusculas
+
+
+
+//'igual a, e do mesmo tipo' e 'diferente de, e do mesmo tipo'
+console.log(age === 31)//sinal de comparação estrita
+//SEMPRE VERIFICA O VALOR E O TIPO
+
+console.log(age !== 31)//diferente de 
+
+//conversao de tipos
+let score = '100'
+score = Number(score)//converte para numero
+console.log(score + 1)
+console.log(typeof score)
+
+
+const crazyConvertion = Number('Maça')
+console.log(crazyConvertion)
+
+covertNumber = String(97)
+const booleanConversion = Boolean(10)
+console.log(10)
+
+/*
+Valores falsy
+- false
+- 0
+- "", '', ``
+- null
+- undefinened
+- NaN
+
+Valores truthy
+-Qualquer valor que não é falsy
+*/
+
+console.log(Boolean('0'))
